72a80920a0df384e4f216792a428ee5778c784c8
Anthony C <agcarat@gmail.com>
2011-12-28 16:34:55 -0500

    updated readme

--- ut_config.h
+++ ut_config.h
@@ -11,6 +11,8 @@ typedef struct _ut_configuration_t {
 	char rerun_in_debugger;
 	char run_perf_tests;
 	char stop_after_one_failed_test;
+	char run_forked; 
+	char * run_only_test;
 } ut_configuration_t;
 #define UT_CONFIGURATION_DEFAULT {argv[0], stdout,NULL,0,NULL,0,0,0}
 #endif
